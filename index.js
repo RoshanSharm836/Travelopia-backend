9fa337dadec97511fb6fb8eb60ca24b8ceb99932
Roshanrocked <roshna837kumar@gmail.com>
2023-04-04 22:52:40 +0530

    done

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const app = express();
 var cors = require("cors");
 const connection = require("./db/config");
 const router = require("./router/data.model");
+
+const PORT = process.env.PORT || 8000;
+
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
 
-app.listen(3001, () => {
-  console.log("listening on port 3001");
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
   connection();
 });
